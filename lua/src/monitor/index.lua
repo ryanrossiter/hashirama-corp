671500fe6459f8bcfba79baeea917123834d3429
Ryan Rossiter <nayr0788@gmail.com>
2021-11-27 17:59:31 -0500

    modem

--- lua/src/monitor/index.lua
+++ lua/src/monitor/index.lua
@@ -3,9 +3,26 @@ SIDES = {"front", "back", "left", "right", "top", "bottom"}
 monitor = nil
 for i,side in ipairs(SIDES) do
   local present = peripheral.isPresent(side)
-  if present and peripheral.getType(side) == "monitor" then
-    print("Found monitor on side "..side)
-    monitor = peripheral.wrap(side)
+  if present then
+    local type = peripheral.getType(side)
+    if type == "monitor" then
+      print("Found monitor on side "..side)
+      monitor = peripheral.wrap(side)
+    elseif type == "modem" then
+      print("Found modem on side "..side)
+      local modem = peripheral.wrap(side)
+      for i,name in ipairs(model.getNamesRemote()) do
+        local rtype = modem.getTypeRemote(name)
+        if rtype == "monitor" then
+          print("Found monitor on modem "..name)
+          monitor = peripheral.wrap(side)
+        end
+      end
+    end
+  end
+
+  if monitor then
+    break
   end
 end
 
@@ -14,7 +31,7 @@ monitor.setTextScale(0.5)
 
 local oldterm = term.redirect(monitor)
 -- Now all term calls will go to the monitor instead
-local image = paintutils.loadImage("happy.put")
+local image = paintutils.loadImage("happy.nfp")
 paintutils.drawImage(image, 1, 1)
 term.redirect( oldterm )
 -- Now the term.* calls will draw on the terminal again
