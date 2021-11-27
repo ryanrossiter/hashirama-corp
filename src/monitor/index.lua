50482a5dbe923beab2639da3b75788c1c0625bf0
Ryan Rossiter <nayr0788@gmail.com>
2021-11-27 03:34:27 -0500

    moar

---
+++ src/monitor/index.lua
@@ -0,0 +1,10 @@
+SIDES = {"front", "back", "left", "right", "top", "bottom"}
+
+for i,side in ipairs(SIDES) do
+  local present = peripheral.isPresent(side)
+  if present do
+    print("Found peripheral "..peripheral.getType(side).." on side "..side)
+  end
+end
+
+peripheral.call( "left", "write", "Hello World!" )
